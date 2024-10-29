c7adae3dd78902c9dd925a4c104499042fdadff8
juan carlos macias <juancmaciassalvador@gmail.com>
2024-10-29 10:59:10 +0100

    Inicial

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# reviews_google
+# reviews_google 
+proyecto para mostrar las reseñas en Google mapa de los usuarios.
+- desarrollado en php
+- uso de api de google map
+
+Antees de comenzar debemos saber el id particular del comercio para ello debemos ir:
+[Buscar id comercio](https://developers.google.com/maps/documentation/places/web-service/place-id?hl=es-419#places-api)
+![Pantalla de busqueda](<img/buscar_id.JPG>)
+Me he buscado, ese es el resultado.
+mi 'place_id' es 'ChIJxQQ8oG0nQg0Rg-0sKaNugIc'.
+No es necesario buscarlo ya que hay otra api que podemos implementar y nos devuelva el place_id. Eso sera en otro momento.
+una vez que lo tenemos, necesitaremos nuestro 'API_KEY' que nos los proporciona desde nuestro panel de Google.
+Ahora solo nos queda montar la url con estos dos datos:
+- place_id = ChIJxQQ8oG0nQg0Rg-0sKaNugIc
+- key = YOUR_API_KEY
+Y quedaría así:
+ https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJrTLr-GyuEmsRBfy61i59si0&key=YOUR_API_KEY
+
+ Con esto nos devolvera un json si todo esta correcto, similar:
+ ![Respuesta json](img/json.JPG)
+
+ Ya solo nos quedaría procesar el json para mostrar la información que  queremos.
+ En mi caso voy a utilizar php, pero podemos hacer la petición en el lenguaje que más nos guste.
+
+ Solo tengo una reseña, pero puede ver como funciona en mi personal page.
+
+ Si te gusto dale una estrella y puede invitarme a un café.
+
+<a href="https://www.buymeacoffee.com/juancmaciau" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-blue.png" alt="Invitarme a café" style="height: 60px !important;width: 217px !important;" ></a>
+
+O dejarme una reseña en:
+[Deja tu reseña](https://g.page/r/CYPtLCmjboCHEAE/review)
+
---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Reviews on Google, example php</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="cliente">
+            <?php
+            $data = json_decode(file_get_contents("https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJxQQ8oG0nQg0Rg-0sKaNugIc&fields=name,rating,reviews&reviews_no_translations=false&translated=false&key=AIzaSyB1yxZO16d0XSdQyVhOYniYyJ-jCNRr92Y"), true);
+            if ($data && isset($data['result']['reviews'])) {
+                echo '
+                        <h1>Reseñas para: ' . $data['result']['name'] . '</h1>
+                        <div class="def">';
+
+                echo $data['result']['rating'];
+                echo '<div class="estrellas">';
+                for ($i = 0; $i < $data['result']['rating']; $i++) {
+                    echo '<span class="gral" ><i class="fa-solid fa-star"></i></span>';
+                }
+                $total = count($data['result']['reviews']);
+                echo '</div> de ' . $total . ($total < 1 ? ' reseñas' : ' reseña') . '.</div>';
+                echo '<div class="reviews">';
+                // Iterar sobre las reseñas
+                foreach ($data['result']['reviews'] as $review) {
+                    echo '
+                 <div class="review">
+                <img src="' . $review['profile_photo_url'] . '" alt="Foto de ' . $review['author_name'] . '" class="profile-photo">
+                <h2>' . $review['author_name'] . '</h2>';
+            ?>
+                    <div class="rating">
+                        <i class="fa-regular fa-star"></i>
+                        <span class="kvMYJc" role="img" aria-label="5 estrellas">
+                            <?php
+                            $res = 5 - $review['rating'];
+                            for ($i = 1; $i < $review['rating']; $i++) {
+                            ?>
+                                <span class="hCCjke  NhBTye elGi1d"><i class="fa-solid fa-star"></i></span>
+                            <?php
+                            }
+                            for ($i = 1; $i < $res; $i++) {
+                            ?>
+                                <span class="neutro"><i class="fa-regular fa-star"></i></span>
+                            <?php
+                            }
+                            ?>
+
+                        </span>
+                        <span class="rsqaWe"><?php echo $review['relative_time_description'] ?></span>
+                        <div class="Tuisuc"></div>
+                    </div>
+            <?php
+                    echo '
+                <p class="text">' . $review['text'] . '</p>
+                <a href="' . $review['author_url'] . '" target="_blank">Ver más</a>
+                </div>
+                    ';
+                }
+            } else {
+                echo "No se pudieron obtener las reseñas.";
+            }
+            ?>
+        </div>
+    </div>
+</div>
+</body>
+</html>
