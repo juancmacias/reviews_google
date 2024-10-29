bf8708ddbf7dda71d8fec214055a8ae4054036b9
Juan Carlos <53483587+juancmacias@users.noreply.github.com>
2024-10-29 11:06:16 +0100

    CHANGE API_KEY

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <div class="container">
         <div class="cliente">
             <?php
-            $data = json_decode(file_get_contents("https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJxQQ8oG0nQg0Rg-0sKaNugIc&fields=name,rating,reviews&reviews_no_translations=false&translated=false&key=AIzaSyB1yxZO16d0XSdQyVhOYniYyJ-jCNRr92Y"), true);
+            $data = json_decode(file_get_contents("https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJxQQ8oG0nQg0Rg-0sKaNugIc&fields=name,rating,reviews&reviews_no_translations=false&translated=false&key=YOUR_API_KEY"), true);
             if ($data && isset($data['result']['reviews'])) {
                 echo '
                         <h1>Reseñas para: ' . $data['result']['name'] . '</h1>
